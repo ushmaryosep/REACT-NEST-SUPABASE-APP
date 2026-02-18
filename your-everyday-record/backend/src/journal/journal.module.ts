55707be663de9df7f8c042fcf30c74c2f272099e
Joshmar Clavero <jlclavero@student.apc.edu.ph>
2026-02-18 15:28:47 +0000

    removed nest

--- your-everyday-record/backend/src/journal/journal.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { JournalController } from './journal.controller';
-import { JournalService } from './journal.service';
-
-@Module({
-  controllers: [JournalController],
-  providers: [JournalService],
-})
-export class JournalModule {}
